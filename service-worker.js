5b1f25f3d68a8e9389dc71b79f8a4479b2dc41f7
rindikartshop <rindikartshop@gmail.com>
2026-08-27 09:52:12 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='rindik-art-v2';
+const CACHE_NAME='rindik-art-v3';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
