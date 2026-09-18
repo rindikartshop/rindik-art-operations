762430a897ddba8ad6792346c6921e4b6fc270b1
rindikartshop <rindikartshop@gmail.com>
2026-09-19 06:42:20 +0700

    Refresh cache for final logo

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='rindik-art-v20260919-13';
+const CACHE='rindik-art-v20260919-14';
 const ASSETS=['./','./index.html','./admin.css','./admin-fix.css','./supabase-config.js','./admin.js?v=20260919-11','./manifest.webmanifest','./rindik-art-logo.svg','./rindik-art-icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
