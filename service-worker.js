38734a3f96f763881c2a60ce54362271a2ea0f76
rindikartshop <rindikartshop@gmail.com>
2026-09-18 22:08:53 +0700

    Refresh service worker cache for complete app

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='rindik-art-v20260918-2';
+const CACHE='rindik-art-v20260918-3';
 const ASSETS=['./','./index.html','./admin.css','./admin-fix.css','./supabase-config.js','./admin.js','./manifest.webmanifest','./rindik-art-logo.svg','./rindik-art-icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
