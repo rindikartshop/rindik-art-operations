b9831e5e9a78bb34b540c9a1c5db5ba703e5efa0
rindikartshop <rindikartshop@gmail.com>
2026-09-18 17:45:42 +0700

    Update Supabase URL in configuration

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.RINDIK_SUPABASE = {
-  url: 'https://bucrnlvwcyhsovsgwhkz.supabase.co',
+  url:'https://bucmlvvwsgwhkz.supabase.co'.
   publishableKey: 'sb_publishable_c5WWLAtiycadHSvNvmUHHw_4FTyx4Gk'
 };
