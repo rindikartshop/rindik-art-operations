3c1185b5df61ad097b79e8925e609f1600657365
rindikartshop <rindikartshop@gmail.com>
2026-09-19 11:07:20 +0700

    Disable unstable PWA service worker for operational stability

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,2 @@
-const CACHE='rindik-art-v20260919-21';
-const ASSETS=['./','./index.html','./admin.css','./admin-fix.css','./supabase-config.js','./admin.js?v=20260919-19','./manifest.webmanifest','./rindik-art-logo.svg','./rindik-art-icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
-  const u=new URL(e.request.url);
-  if(u.origin===location.origin)e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
-});
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(self.registration.unregister().then(()=>self.clients.matchAll()).then(cs=>Promise.all(cs.map(c=>c.navigate(c.url)))));
